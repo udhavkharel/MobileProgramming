80cd9b28afa07a7de2856ccc783535301512b714
UdhavKharel <udhavkharel8@gmail.com>
2025-11-10 09:21:17 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,91 @@
+
+function Calculate(){
+    let num1 = 5;
+    let num2 = 10;
+    return sum = num1 + num2;   
+}
+
+function display()
+{
+    document.getElementById("addition").innerHTML = Calculate();
+}
+
+function Calculate1(){
+    let num1 = 50;
+    let num2 = 10;
+    return sum = num1 - num2;   
+}
+
+function display1()
+{
+    document.getElementById("substraction").innerHTML = Calculate1();
+}
+
+function Calculate2(){
+    let num1 = 5;
+    let num2 = 3;
+    return sum = num1 * num2;   
+}
+
+function display2()
+{
+    document.getElementById("multiplication").innerHTML = Calculate2();
+}
+
+function Calculate3(){
+    let num1 = 15;
+    let num2 = 5;
+    return sum = num1 / num2;   
+}
+
+function display3()
+{
+    document.getElementById("division").innerHTML = Calculate3();
+}
+
+function Calculate(){
+    let num1 = 5;
+    let num2 = 10;
+    return sum = num1 + num2;   
+}
+
+function display()
+{
+    document.getElementById("addition").innerHTML = Calculate();
+}
+
+function Calculate1(){
+    let num1 = 50;
+    let num2 = 10;
+    return sum = num1 - num2;   
+}
+
+function display1()
+{
+    document.getElementById("substraction").innerHTML = Calculate1();
+}
+
+function Calculate2(){
+    let num1 = 5;
+    let num2 = 3;
+    return sum = num1 * num2;   
+}
+
+function display2()
+{
+    document.getElementById("multiplication").innerHTML = Calculate2();
+}
+
+function ControlFunc(){
+    let num1 = 2;
+    let num2 = 5;
+    if (num1 > num2) {
+        return epp = num1 / num2;
+    } else {
+        return epp = num2 * num1;
+    }
+}
+function display4()
+{
+    document.getElementById("Control").innerHTML = ControlFunc();
+}
